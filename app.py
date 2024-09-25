bd462fecb0c55ff41824edaad2b0632f3f0e0a28
herzphi <philipp.herz@cellcentric.net>
2024-09-25 15:31:59 +0200

    test

--- app.py
+++ app.py
@@ -11,7 +11,12 @@ import matplotlib
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
+app.set( 'port', ( process.env.PORT || 5000 ));
 
+// Start node server
+app.listen( app.get( 'port' ), function() {
+  console.log( 'Node server is running on port ' + app.get( 'port' ));
+  });
 matplotlib.use("Agg")
 
 
